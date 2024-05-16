1a95a53f0ad1e6e23dd449e51579d1788172e43d
Lucas <lucasoliveiratnb0@gmail.com>
2024-05-16 15:12:48 -0300

    Adding CustomUserModel and config UserAdmin

--- app/core/tests/test_commands.py
+++
@@ -1,7 +0,0 @@
-"""
-Test custom Django management commands. 
-"""
-from unittest.mock import patch
-from django.core.management import call_command # type: ignore
-from django.db.utils import OperationalError # type: ignore
-from django.test import SimpleTestCase # type: ignore
